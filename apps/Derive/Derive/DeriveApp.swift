4a7ba48f155953aca49ff0764531d4b3120de677
Benno <57860196+bennoinbeta@users.noreply.github.com>
2026-01-06 07:51:32 +0100

    #3 init derive

---
+++ apps/Derive/Derive/DeriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeriveApp.swift
+//  Derive
+//
+//  Created by Benno on 06.01.26.
+//
+
+import SwiftUI
+
+@main
+struct DeriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

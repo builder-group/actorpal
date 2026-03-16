cf1e2bb6e9ebebc9a914447c0067f2260b14c85c
Benno <57860196+bennoinbeta@users.noreply.github.com>
2026-03-16 10:03:30 +0100

    #develop deprecated gazegames and midimarble

--- apps/_deprecated/midimarble/src/routeTree.gen.ts
+++ apps/_deprecated/midimarble/src/routeTree.gen.ts
@@ -9,9 +9,9 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
-import { Route as MidiRouteImport } from './routes/midi'
-import { Route as IndexRouteImport } from './routes/index'
 import { Route as EditorProjectIdRouteImport } from './routes/editor.$projectId'
+import { Route as IndexRouteImport } from './routes/index'
+import { Route as MidiRouteImport } from './routes/midi'
 
 const MidiRoute = MidiRouteImport.update({
   id: '/midi',
@@ -94,8 +94,8 @@ export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
 
-import type { getRouter } from './router.tsx'
 import type { createStart } from '@tanstack/react-start'
+import type { getRouter } from './router'
 declare module '@tanstack/react-start' {
   interface Register {
     ssr: true
